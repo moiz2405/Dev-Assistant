88fe4ed715d868a57d63858f4792c4bbd59d1ac6
moiz2405 <km856307@gmail.com>
2025-04-01 14:22:18 +0000

    Update: backend/app/query_processor.py

--- backend/app/query_processor.py
+++ backend/app/query_processor.py
@@ -1,2 +1,15 @@
+# receives user input determines user intent
+# if general query or question answers it using a call to chatbot.py else determines the fucntion needed and call it 
+
+
 # determines user intent
 # call required function to take action
+# types of actions or functions to create 
+# 1)project / environment setup - new or existing 
+# 2) file handling - search or open a file
+# 3) apps - open or close an app
+# 4) pdf summarizer - summarizes and pdf 
+# 5) research on a topic 
+# 6)general query or question
+
+
