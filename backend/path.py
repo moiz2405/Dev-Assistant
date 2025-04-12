548bcf664f118674dda5004ed50026f9603b4831
moiz2405 <km856307@gmail.com>
2025-04-12 15:37:46 +0000

    Update: backend/path.py

--- backend/path.py
+++ backend/path.py
@@ -1,3 +1,14 @@
 from app.functions.app_handling import close_app
-input = input()
-close_app(input)
+
+from app.query_processor import determine_function
+from app.models.groq_preprocess import process_query
+from app.functions.file_handler import open_file
+
+input = "can u open a pdf names Linux papers from downloads"
+t = 10
+query = process_query(input)
+determine_function(query)
+# while(t):
+#     t = t-1
+# open_file(input)
+# close_app(input)
