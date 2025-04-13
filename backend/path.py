19c2781dab24e8678e60f38ddf9eebd794f89219
moiz2405 <km856307@gmail.com>
2025-04-13 01:23:31 +0000

    Update: backend/path.py

--- backend/path.py
+++ backend/path.py
@@ -4,10 +4,14 @@ from app.query_processor import determine_function
 from app.models.groq_preprocess import process_query
 from app.functions.file_handler import open_file
 
-input = "can u open a pdf names Linux papers from downloads"
-t = 10
-query = process_query(input)
-determine_function(query)
+# input = "can u open a pdf names Linux papers from downloads"
+# t = 10
+# query = process_query(input)
+# determine_function(query)
+
+from app.tts.test import VoiceAssistant
+assistant = VoiceAssistant()
+assistant.start_hotword_listener()
 # while(t):
 #     t = t-1
 # open_file(input)
