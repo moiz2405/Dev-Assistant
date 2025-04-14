3dc66364674510889ca581fb52d2367321a5e74a
moiz2405 <km856307@gmail.com>
2025-04-14 15:21:41 +0000

    Update: backend/app/query_wrapper.py

--- backend/app/query_wrapper.py
+++
@@ -1,6 +0,0 @@
-from app.models.groq_preprocess import process_query
-from app.query_processor import determine_function
-
-def query_action_wrapper(text):
-    processed_query = process_query(text)
-    determine_function(processed_query)
