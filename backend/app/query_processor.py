b83f3f8043993ffa1c34af1ffa6c0b8d6ab511ba
moiz2405 <km856307@gmail.com>
2025-04-15 15:08:05 +0000

    Update: backend/app/query_processor.py

--- backend/app/query_processor.py
+++ backend/app/query_processor.py
@@ -1,14 +1,5 @@
 # receives user input determines user intent
 # if general query or question answers it using a call to chatbot.py else determines the fucntion needed and call it 
-# determines user intent
-# call required function to take action
-# types of actions or functions to create 
-# 1)project / environment setup - new or existing 
-# 2) file handling - search or open a file
-# 3) apps - open or close an app
-# 4) pdf summarizer - summarizes and pdf 
-# 5) research on a topic 
-# 6)general query or question
 
 from app.functions.app_handling import open_app, close_app
 from app.functions.environment_setup import setup_project #takes repo link and target dir as argu, will refine later
