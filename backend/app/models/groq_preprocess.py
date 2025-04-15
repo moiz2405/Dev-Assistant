081e6d78a35020ed438f605fbaedb0eb78669012
moiz2405 <km856307@gmail.com>
2025-04-15 15:02:01 +0000

    Update: backend/app/models/groq_preprocess.py

--- backend/app/models/groq_preprocess.py
+++ backend/app/models/groq_preprocess.py
@@ -11,39 +11,10 @@ import platform
 import os
 import getpass
 import diskcache
+# from query_types import QueryType, SubTaskType
+from app.models.query_types import QueryType,SubTaskType
 cache = diskcache.Cache(".query_cache")
 
-class QueryType(str, Enum):
-    GITHUB_ACTIONS = "GITHUB_ACTIONS"         
-    PROJECT_SETUP = "PROJECT_SETUP"             
-    FILE_HANDLING = "FILE_HANDLING"             
-    APP_HANDLING = "APP_HANDLING"               
-    SUMMARIZER = "SUMMARIZER"                  
-    GENERAL_QUERY = "GENERAL_QUERY"
-class SubTaskType(str, Enum):
-    # GitHub Actions
-    LIST_REPOS = "LIST_REPOS"
-    CLONE_REPO = "CLONE_REPO"
-    CREATE_AND_PUSH = "CREATE_AND_PUSH"
-
-    # Project Setup
-    NEW_PROJECT = "NEW_PROJECT"
-    EXISTING_PROJECT = "EXISTING_PROJECT"
-
-    # File Handling
-    SEARCH_FILE = "SEARCH_FILE"
-    OPEN_FILE = "OPEN_FILE"
-    CLOSE_FILE = "CLOSE_FILE"
-
-    # App Handling
-    OPEN_APP = "OPEN_APP"
-    CLOSE_APP = "CLOSE_APP"
-
-    # Summarizer
-    SUMMARIZE = "SUMMARIZE"
-    ANSWER_QUERY = "ANSWER_QUERY"
-    #general 
-    GENERAL_QUERY = "GENERAL_QUERY"
 
 class QueryProcessor(BaseModel):
     type: QueryType = Field(
