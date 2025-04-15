3172c1b8e4b3cb4e5c0c325739bbc697f067891a
moiz2405 <km856307@gmail.com>
2025-04-15 14:50:10 +0000

    Update: backend/app/models/groq_preprocess.py

--- backend/app/models/groq_preprocess.py
+++ backend/app/models/groq_preprocess.py
@@ -7,13 +7,43 @@ from pydantic import BaseModel, Field
 from agno.agent import Agent, RunResponse
 from agno.models.groq import Groq
 from pathlib import Path
-from types import QueryType,SubTaskType
 import platform
 import os
 import getpass
 import diskcache
-
 cache = diskcache.Cache(".query_cache")
+
+class QueryType(str, Enum):
+    GITHUB_ACTIONS = "GITHUB_ACTIONS"         
+    PROJECT_SETUP = "PROJECT_SETUP"             
+    FILE_HANDLING = "FILE_HANDLING"             
+    APP_HANDLING = "APP_HANDLING"               
+    SUMMARIZER = "SUMMARIZER"                  
+    GENERAL_QUERY = "GENERAL_QUERY"
+class SubTaskType(str, Enum):
+    # GitHub Actions
+    LIST_REPOS = "LIST_REPOS"
+    CLONE_REPO = "CLONE_REPO"
+    CREATE_AND_PUSH = "CREATE_AND_PUSH"
+
+    # Project Setup
+    NEW_PROJECT = "NEW_PROJECT"
+    EXISTING_PROJECT = "EXISTING_PROJECT"
+
+    # File Handling
+    SEARCH_FILE = "SEARCH_FILE"
+    OPEN_FILE = "OPEN_FILE"
+    CLOSE_FILE = "CLOSE_FILE"
+
+    # App Handling
+    OPEN_APP = "OPEN_APP"
+    CLOSE_APP = "CLOSE_APP"
+
+    # Summarizer
+    SUMMARIZE = "SUMMARIZE"
+    ANSWER_QUERY = "ANSWER_QUERY"
+    #general 
+    GENERAL_QUERY = "GENERAL_QUERY"
 
 class QueryProcessor(BaseModel):
     type: QueryType = Field(
