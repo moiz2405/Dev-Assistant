ce6867784e3f70ababaf82a038f11ae2efaf0b62
moiz2405 <km856307@gmail.com>
2025-04-15 01:05:18 +0000

    Update: test.py

--- backend/test.py
+++ backend/test.py
@@ -3,5 +3,5 @@ import os; print("USERNAME:", os.getenv("GITHUB_USERNAME"))
 
 from app.functions.github_handler import push_folder_to_github
 
-push_folder_to_github("test_repo", "C:\\Users\\Shikhar\\Downloads\\test")
+push_folder_to_github("test_repo", "/mnt/c/Users/km866/Downloads/test_folder")
 
