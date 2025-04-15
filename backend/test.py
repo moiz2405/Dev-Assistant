c60a754064c3849f8b68e81c626685d362c75408
moiz2405 <km856307@gmail.com>
2025-04-15 01:56:43 +0000

    Update: backend/test.py

--- backend/test.py
+++ backend/test.py
@@ -1,7 +1,10 @@
 import os; print("USERNAME:", os.getenv("GITHUB_USERNAME"))
 
 
-from app.functions.github_handler import push_folder_to_github
+from app.functions.github_handler import push_folder_to_github, list_github_repos, search_repo_url
 
-push_folder_to_github("test_repo", "/mnt/c/Users/km866/Downloads/test_folder")
+# push_folder_to_github("test_repo", "/mnt/c/Users/km866/Downloads/test_folder")
+# list_github_repos()
 
+repo_url = search_repo_url("acc")
+print(repo_url)
