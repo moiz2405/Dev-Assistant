3dba63b07799a3a22379c1a67c2758f3c1cdae9a
moiz2405 <km856307@gmail.com>
2025-04-16 14:33:34 +0000

    Update: backend/test.py

--- backend/test.py
+++ backend/test.py
@@ -13,19 +13,20 @@ from app.models.groq_preprocess import cached_process_query
 from app.stt.voice_recognition import VoiceAssistant
 # from app.models.test_preprocess import cached_process_query
 from app.query_processor import determine_function
-# while(1):
-#     prompt = input()
-#     determine_function(cached_process_query(prompt))
-def handle_recognized_command(text):
-    if not text:
-        print("[MAIN] Nothing recognized.")
-        return
+while(1):
+    prompt = input()
+    cached_process_query(prompt)
+    # determine_function(cached_process_query(prompt))
+# def handle_recognized_command(text):
+#     if not text:
+#         print("[MAIN] Nothing recognized.")
+#         return
 
-    print(f"[MAIN] Recognized: {text}")
-    
-assistant = VoiceAssistant(
-    hotword="jarvis",
-    record_duration=5,
-    on_recognized=handle_recognized_command
-)
-assistant.start_hotword_listener()
+#     print(f"[MAIN] Recognized: {text}")
+
+# assistant = VoiceAssistant(
+#     hotword="jarvis",
+#     record_duration=5,
+#     on_recognized=handle_recognized_command
+# )
+# assistant.start_hotword_listener()
