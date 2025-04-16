2ed90a67c24fb96bae96f28e61266d9dbf9e99b7
moiz2405 <km856307@gmail.com>
2025-04-16 15:20:25 +0000

    Update: backend/test.py

--- backend/test.py
+++ backend/test.py
@@ -10,18 +10,20 @@
 # print(repo_url)
 import os
 import sys
-def suppress_stderr():
-    devnull = os.open(os.devnull, os.O_WRONLY)
-    os.dup2(devnull, sys.stderr.fileno())
+# def suppress_stderr():
+#     devnull = os.open(os.devnull, os.O_WRONLY)
+#     os.dup2(devnull, sys.stderr.fileno())
 
-suppress_stderr()
-from app.models.groq_preprocess import cached_process_query
-from app.stt.voice_recognition import VoiceAssistant
-# from app.models.test_preprocess import cached_process_query
-from app.query_processor import determine_function
-while(1):
-    prompt = input()
-    cached_process_query(prompt)
+# suppress_stderr()
+# from app.models.groq_preprocess import cached_process_query
+# from app.stt.voice_recognition import VoiceAssistant
+# # from app.models.test_preprocess import cached_process_query
+# from app.query_processor import determine_function
+# while(1):
+#     prompt = input()
+#     cached_process_query(prompt)
+
+
     # determine_function(cached_process_query(prompt))
 # def handle_recognized_command(text):
 #     if not text:
@@ -36,3 +38,10 @@ while(1):
 #     on_recognized=handle_recognized_command
 # )
 # assistant.start_hotword_listener()
+from app.models.path_processor import list_directory_contents
+while(1):
+      user_path = input()
+      contents = list_directory_contents(user_path)
+      print(f"\nContents of '{user_path}':")
+      for item in contents:
+          print(f"[{item['type'].upper()}] {item['name']}")
