7595a6af7abf74233d6c53f8dda9c1e341ead3d5
moiz2405 <km856307@gmail.com>
2025-04-20 15:11:51 +0000

    Update: backend/app/query_processor.py

--- backend/app/query_processor.py
+++ backend/app/query_processor.py
@@ -2,7 +2,7 @@
 # if general query or question answers it using a call to chatbot.py else determines the fucntion needed and call it 
 
 from app.functions.app_handling import open_app, close_app
-from app.functions.environment_setup import setup_project #takes repo link and target dir as argu, will refine later
+from app.functions.environment_setup import setup_project
 from app.functions.file_handler import open_file,list_files_by_type
 from app.functions.summarizer import summarize_in_new_window
 from app.functions.github_handler import clone_github_repo, list_github_repos, push_folder_to_github
@@ -49,17 +49,6 @@ def determine_function(structured_query):
 # def determine_sub_type(structured_query):
     
 
-# def determine_function(structured_query):
-#     #for app handling 
-#     open_app(app_name)
-#     close_app(app_name)
-#     #for environment setup 
-#     setup_project(repo_link,target_directory)
-#     #for file handler
-#     search_file(file_name, search_path)
-#     open_file(file_path)
-#     move_file(file_path, new_location)
-    
 
 
 
