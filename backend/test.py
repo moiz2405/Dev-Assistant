5f467a93284b990c92f5a807c83cabb0aaa316a8
moiz2405 <km856307@gmail.com>
2025-05-31 17:02:58 +0000

    Update: backend/test.py

--- backend/test.py
+++ backend/test.py
@@ -4,12 +4,22 @@ import sys
 import atexit
 
 
-from app.stt.voice_recognition import VoiceAssistant
-from app.models.groq_preprocess import cached_process_query
-from app.query_processor import determine_function
-from app.tts.response_generator import generate_response
-# from app.tts.eleven_labs_tts import speak
-from app.tts.edge_tts import speak_text
+# from app.stt.voice_recognition import VoiceAssistant
+# from app.models.groq_preprocess import cached_process_query
+# from app.query_processor import determine_function
+# from app.tts.response_generator import generate_response
+# # from app.tts.eleven_labs_tts import speak
+# from app.tts.edge_tts import speak_text
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+from shared_todo_queue import enqueue,dequeue
+
+# enqueue("Hello")
+# enqueue("Hello")
+task = dequeue()
+print(task)
+# enqueue("another hello")
+
 
 # executor = ThreadPoolExecutor(max_workers=4)
 # atexit.register(lambda: executor.shutdown(wait=True))
@@ -28,6 +38,6 @@ from app.tts.edge_tts import speak_text
 # assistant = VoiceAssistant(hotword="vision",record_duration=6,on_recognized=handle_recognized_command)
 # assistant.start_hotword_listener()
 
-while(1):
-    prompt = input("Enter your prompt")
-    determine_function(cached_process_query(prompt))
+# while(1):
+#     prompt = input("Enter your prompt")
+#     determine_function(cached_process_query(prompt))
