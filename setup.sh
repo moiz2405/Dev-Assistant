d0f43bcfdcb6e0d3296af5b7bd73168cffb77f1d
moiz2405 <km856307@gmail.com>
2025-06-01 17:42:13 +0000

    Update: setup.sh

--- setup.sh
+++ setup.sh
@@ -4,6 +4,8 @@
 source venv/bin/activate
 
 # Export the API keys from .env.local
+export GITHUB_USERNAME=$(grep '^GITHUB_USERNAME=' .env.local | cut -d '=' -f2- | xargs)
+export GITHUB_TOKEN=$(grep '^GITHUB_TOKEN=' .env.local | cut -d '=' -f2- | xargs)
 export GROQ_API_KEY=$(grep '^GROQ_API_KEY=' .env.local | cut -d '=' -f2- | xargs)
 export GEMINI_API_KEY=$(grep '^GEMINI_API_KEY=' .env.local | cut -d '=' -f2- | xargs)
 # export ELEVENLABS_API_KEY=$(grep '^ELEVENLABS_API_KEY=' .env.local | cut -d '=' -f2- | xargs)
@@ -12,6 +14,7 @@ export GEMINI_API_KEY=$(grep '^GEMINI_API_KEY=' .env.local | cut -d '=' -f2- | x
 echo "Virtual environment activated."
 echo "GROQ_API_KEY set"
 echo "GEMINI_API_KEY set"
+echo "GITHUB CREDS set"
 # echo "ELEVENLABS_API_KEY set "
 
 # Don't exec $SHELL unless you have a reason.
